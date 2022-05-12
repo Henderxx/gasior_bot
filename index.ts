6bb0c24cb6c711b5ac9a5f3e23747f3cd593879d
lukasz-pc <henderxx@gmail.com>
2022-05-12 23:27:04 +0200

    added messages

--- index.ts
+++ index.ts
@@ -1,38 +1,46 @@
 import { Client, Intents, Options } from 'discord.js'
-import dotnenv from 'dotenv'
+import * as dotnenv from 'dotenv'
 dotnenv.config()
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES], makeCache: Options.cacheEverything() })
 
 client.once('ready', () => {
-    console.log('Gąsior przybył aby cię nawiedzić')
+  console.log('Gąsior przybył aby cię nawiedzić')
 })
 
 client.on('ready', (client) => {
-    const guild = client.guilds.cache.get(`${process.env.GUILD}`)
-    sendRandomMesage(guild)
+  const guild = client.guilds.cache.get(`${process.env.GUILD}`)
+  sendRandomMesage(guild)
 })
 
 client.login(process.env.TOKEN)
 
-const messagesList = ['piersza wiadomosc', 'druga wiadomosc', 'trzecia wiadomosc']
+const messagesList = [
+  'Jeśli w dniu zakochanych nie jesteś zakochany nie martw się, na święto zmarłych też nie musisz być martwy',
+  '*....Szur szur szur, coś tu pust ostanio ...*',
+  'Heeej..... hej Ty, Powiedz temu za barem żeby mnie wypuścił ',
+  'Oo nowy, a ciebie za co tu zrzucili?',
+  'Każdy tylko wpada i wypada, ani się nie przywita ani Bimbru nie przyniesie',
+  'Uciszcie tego Barda tam na górze!!!! .... nawet mi uszy już więdną..',
+  '...I kolejnego niosą tu nieprzytomnego...Chodź mam dla ciebie całe trzy minuty, potem i tak ci nikt nie uwierzy.... ',
+]
 
 function generateRandomTime(min: number = 1, max: number = 60) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 function selectRandomMessage() {
-    const messageNumber = generateRandomTime(1, messagesList.length)
-    return messageNumber
+  const messageNumber = generateRandomTime(1, messagesList.length)
+  return messageNumber
 }
 
 function sendRandomMesage(guild: any) {
-    const messageChannel: any = guild?.channels.cache.get(`${process.env.MESSAGE_CHANNEL}`)
-    const message = messagesList[selectRandomMessage() - 1]
-    messageChannel.send(message)
-    const randomRepeatTime = generateRandomTime() * 1000
-    console.log(randomRepeatTime)
-    setTimeout(sendRandomMesage, randomRepeatTime, guild)
+  const messageChannel: any = guild?.channels.cache.get(`${process.env.MESSAGE_CHANNEL}`)
+  const message = messagesList[selectRandomMessage() - 1]
+  messageChannel.send(message)
+  const randomRepeatTime = generateRandomTime() * 1000 * 60 * 30
+  console.log(randomRepeatTime)
+  setTimeout(sendRandomMesage, randomRepeatTime, guild)
 }
